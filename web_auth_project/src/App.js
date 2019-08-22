e5994bf5b7a70641920d3343547931da59592676
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-08-21 21:17:20 -0600

    Sorry for the single comment for htis challenge. I completed the MVP early so I started working on the CRA and I did the middleware function as part of the MVP so I did that already.

---
+++ web_auth_project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
